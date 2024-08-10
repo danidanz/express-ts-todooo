71e3ab512d8d27cbe51ed55fddc55659edcc2107
Dani Satria <danidanz@gmail.com>
2024-08-11 02:16:06 +0700

    fix: some auth middleware console log updates

--- src/middlewares/authMiddleware.ts
+++ src/middlewares/authMiddleware.ts
@@ -9,30 +9,30 @@ export const auth = (req: Request, res: Response, next: NextFunction) => {
   if (accessToken) {
     try {
       jwt.verify(accessToken, process.env.JWT_ACCESS_TOKEN_SECRET as string);
-      console.log("Access token masih valid");
+      console.log("Access token is still valid");
       next();
     } catch (error) {
       // If false, regenerate new access token from refreshToken
       if (!refreshToken) {
-        console.log("Refresh Token tidak ada");
+        console.log("Refresh Token is not found");
         return res.status(401).json({ message: "Please re-login..." });
       }
 
       try {
         // Check if Refresh Token Valid
-        console.log("Verifikasi Refresh Token");
+        console.log("Verify Refresh Token");
         jwt.verify(
           refreshToken,
           process.env.JWT_REFRESH_TOKEN_SECRET as string
         );
         // If valid, verify if it's exist in database
-        console.log("Cek refresh token ke database");
+        console.log("Checking refresh token into the database");
         const activeRefreshToken = Auth.findOne({
           refreshToken,
         });
 
         if (!activeRefreshToken) {
-          console.log("Refresh token tidak ada di database");
+          console.log("Refresh token can't be found in the database");
           return res.status(401).json({ message: "Please re-login..." });
         }
 
@@ -52,10 +52,9 @@ export const auth = (req: Request, res: Response, next: NextFunction) => {
           process.env.JWT_ACCESS_TOKEN_SECRET as string,
           { expiresIn: 300 }
         );
-
+        // regenerate new access token
         res.cookie("accessToken", newAccessToken, { httpOnly: true });
         next();
-        // regenerate new access token
       } catch (error) {
         // If invalid, user need to re-login
         return res.status(401).json({ message: "Please re-login..." });
